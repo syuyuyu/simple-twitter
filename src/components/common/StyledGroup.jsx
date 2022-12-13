7c135e707bda909c9dcb8e1e82e8bcfe4b36ffd6
BearK <harry.hsu0515@gmail.com>
2022-12-13 15:58:23 +0800

    add groupcontainer style

--- src/components/common/StyledGroup.jsx
+++ src/components/common/StyledGroup.jsx
@@ -1,1 +1,13 @@
 import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 356px;
+  height: 100vh;
+  margin: 0 auto;
+  padding-top: 64px;
+`;
+
+export { StyledContainer as GroupContainer };
