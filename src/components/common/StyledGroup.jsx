00b77c5e46ea33d1b506a7e01cc470278d64c61f
BearK <harry.hsu0515@gmail.com>
2022-12-13 16:12:49 +0800

    add repo-namel

--- src/components/common/StyledGroup.jsx
+++ src/components/common/StyledGroup.jsx
@@ -2,12 +2,7 @@ import styled from "styled-components";
 
 const StyledContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  max-width: 356px;
-  height: 100vh;
-  margin: 0 auto;
-  padding-top: 64px;
+  flex-flow: row;
 `;
 
 export { StyledContainer as GroupContainer };
