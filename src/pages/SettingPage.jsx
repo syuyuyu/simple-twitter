26ded1d4b0d14486869de76830bc8cc0029b6583
Syuan Liu <syuan.haha@gmail.com>
2022-12-26 17:45:01 +0800

    debug

--- src/pages/SettingPage.jsx
+++ src/pages/SettingPage.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import { StyledGroupContainer } from "../components/common/StyledGroup";
 import SectionSidebar from "../components/SectionSidebar";
 import Setting from "../components/Setting";
+import TweetModal from "../components/Modals/TweetModal";
+
 
 const SettingPage = () => {
   return (
-    <StyledGroupContainer>
-      <SectionSidebar />
-      <Setting />
-    </StyledGroupContainer>
+    <>
+      <StyledGroupContainer>
+        <SectionSidebar />
+        <Setting />
+      </StyledGroupContainer>
+      <TweetModal />
+    </>
   );
 };
 
